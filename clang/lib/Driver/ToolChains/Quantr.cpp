9dcbee1972b2fd9b5ef5d50b3c55859cdc83f486
Peter <peter@quantr.hk>
2022-09-12 21:43:15 +0800

    dev

--- clang/lib/Driver/ToolChains/Quantr.cpp
+++ clang/lib/Driver/ToolChains/Quantr.cpp
@@ -4,14 +4,20 @@
 #include "clang/Driver/Compilation.h"
 #include "clang/Driver/Options.h"
 #include "llvm/Option/ArgList.h"
+#include <iostream>
 
 using namespace clang::driver;
 using namespace clang::driver::toolchains;
 using namespace llvm::opt;
 using namespace clang;
+using namespace std;
 
-QuantrToolChain::QuantrToolChain(const Driver &D, const llvm::Triple &Triple, const ArgList &Args)
-    : ToolChain(D, Triple, Args) {}
+QuantrToolChain::QuantrToolChain(const Driver &D, const llvm::Triple &Triple,
+                                 const ArgList &Args)
+    : ToolChain(D, Triple, Args) {
+  cout << "QuantrToolChain" << endl;
+}
 
-QuantrToolChain::~QuantrToolChain() {}
-
+QuantrToolChain::~QuantrToolChain() {
+  cout << "QuantrToolChain destructor" << endl;
+}
