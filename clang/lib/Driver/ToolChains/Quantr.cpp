5ef2ef62e7400c30df19f601cc3f00b3feb3e6d8
Peter <peter@quantr.hk>
2022-09-13 00:24:01 +0800

    dev

--- clang/lib/Driver/ToolChains/Quantr.cpp
+++ clang/lib/Driver/ToolChains/Quantr.cpp
@@ -21,3 +21,27 @@ QuantrToolChain::QuantrToolChain(const Driver &D, const llvm::Triple &Triple,
 QuantrToolChain::~QuantrToolChain() {
   cout << "QuantrToolChain destructor" << endl;
 }
+
+auto QuantrToolChain::buildAssembler() const -> Tool * {
+  cout << "buildAssembler" << endl;
+  return NULL;
+}
+
+auto QuantrToolChain::buildLinker() const -> Tool * {
+  cout << "buildLinker" << endl;
+  return new tools::Quantr::Linker(*this);
+}
+
+void tools::Quantr::Linker::constructLinkAndEmitSpirvCommand(
+    Compilation &C, const JobAction &JA, const InputInfoList &Inputs,
+    const InputInfo &Output, const llvm::opt::ArgList &Args) const {
+  cout << "constructLinkAndEmitSpirvCommand" << endl;
+}
+
+void tools::Quantr::Linker::ConstructJob(Compilation &C, const JobAction &JA,
+                                         const InputInfo &Output,
+                                         const InputInfoList &Inputs,
+                                         const ArgList &Args,
+                                         const char *LinkingOutput) const {
+  cout << "ConstructJob" << endl;
+}
--- clang/lib/Driver/ToolChains/Quantr.h
+++ clang/lib/Driver/ToolChains/Quantr.h
@@ -10,9 +10,8 @@ namespace toolchains {
 class LLVM_LIBRARY_VISIBILITY QuantrToolChain : public ToolChain {
 public:
   QuantrToolChain(const Driver &D, const llvm::Triple &Triple,
-      const llvm::opt::ArgList &Args);
+                  const llvm::opt::ArgList &Args);
   ~QuantrToolChain() override;
-
 
   bool isPICDefault() const override { return false; }
   bool isPIEDefault(const llvm::opt::ArgList &Args) const override {
@@ -24,15 +23,41 @@ public:
   bool IsIntegratedAssemblerDefault() const override { return false; }
   bool useIntegratedAs() const override { return false; }
 
-//   void
-//   AddClangSystemIncludeArgs(const llvm::opt::ArgList &DriverArgs,
-//                             llvm::opt::ArgStringList &CC1Args) const override;
+  //   void
+  //   AddClangSystemIncludeArgs(const llvm::opt::ArgList &DriverArgs,
+  //                             llvm::opt::ArgStringList &CC1Args) const
+  //                             override;
 
-// protected:
-//   Tool *buildAssembler() const override;
-//   Tool *buildLinker() const override;
+protected:
+  Tool *buildAssembler() const override;
+  Tool *buildLinker() const override;
 };
+
 } // namespace toolchains
+
+namespace tools {
+namespace Quantr {
+
+class LLVM_LIBRARY_VISIBILITY Linker : public Tool {
+public:
+  Linker(const ToolChain &TC) : Tool("Quantr::Linker", "quantr-link", TC) {}
+
+  bool hasIntegratedCPP() const override { return false; }
+
+  void ConstructJob(Compilation &C, const JobAction &JA,
+                    const InputInfo &Output, const InputInfoList &Inputs,
+                    const llvm::opt::ArgList &TCArgs,
+                    const char *LinkingOutput) const override;
+
+private:
+  void constructLinkAndEmitSpirvCommand(Compilation &C, const JobAction &JA,
+                                        const InputInfoList &Inputs,
+                                        const InputInfo &Output,
+                                        const llvm::opt::ArgList &Args) const;
+};
+} // namespace Quantr
+
+} // namespace tools
 } // namespace driver
 } // namespace clang
 
